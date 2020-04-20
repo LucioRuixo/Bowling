cdc495b3618b897cd07b1068b7ef13843a0fa69e
Lucio Ruixo <lucioruixo@gmail.com>
2020-04-20 01:39:04 -0300

    Basic movement and collisions added

---
+++ TP3 - Bowling/Assets/Scenes/Bowling/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float movementSpeed = 10.0f;
+    float rollForce = 1000.0f;
+
+    bool applyingForce = false;
+
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        CheckInput();
+
+        if (applyingForce)
+            rigidBody.AddForce(Vector3.forward * rollForce * Time.deltaTime);
+
+        Debug.Log(applyingForce);
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetButton("Horizontal"))
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            applyingForce = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Pine"))
+            applyingForce = false;
+    }
+}
---
+++ TP3 - Bowling/Assets/Scenes/Bowling/Scripts/MainCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Vector3 offset;
+
+    public GameObject pivot;
+
+    void Update()
+    {
+        transform.position = pivot.transform.position + offset;
+    }
+}
